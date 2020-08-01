a3c6bb7062b9e4c457be8097be8e774a01629b5c
Akhil Sukhthankar <akhilsukh01@gmail.com>
2020-07-31 23:04:14 -0700

    Added js file and search functionality

---
+++ public/index.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    var search = document.getElementById('search-id')
+
+    search.addEventListener("keyup", function (event) {
+        search.focus()
+        if (event.keyCode === 13) {
+            event.preventDefault()
+            if (search.value === '') {
+                search.placeholder = "Try Again"
+            }
+            else {
+                var searchQuery = "http://www.google.com/search?q=" + search.value
+                window.location.href = searchQuery
+            }
+        }
+    });
+
+}
+
