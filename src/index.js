0d3becc09b66d54adbf543b908ae289e1cc66a8c
Akhil Sukhthankar <akhilsukh01@thinkpad.attlocal.net>
2020-08-01 19:20:19 -0700

    Added code to erase search query on click

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ window.onload = function () {
             else {
                 var searchQuery = "http://www.google.com/search?q=" + search.value
                 window.location.href = searchQuery
+                // search.value = null
             }
         }
     });
